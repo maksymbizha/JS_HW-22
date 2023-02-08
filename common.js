ab8bf7a2ccf2a7e1afd528f2cd58b69c498cf96d
Maksym Bizhanov <103897410+maksymbizha@users.noreply.github.com>
2023-02-08 20:38:13 +0200

    update

--- common.js
+++ common.js
@@ -13,65 +13,30 @@ const questions = [{
     {
         caption: 'Припинити спливання події можна за допомогою метода stopPropagation?',
         correctAnswer: true
-    }]
 
+    }
+    ]
 
 
 /*------- ul --------*/
 const ulElement = document.querySelector('ul');
 
-/*------- li --------*/
-const oneLiElement=document.createElement('li')
-ulElement.append(oneLiElement);
+/*------- li , label , input--------*/
+for (let i = 0; i <questions.length; i ++){
+    const LiElement=document.createElement('li')
+    ulElement.append(LiElement);
 
-const twoLiElement=document.createElement('li')
-ulElement.append(twoLiElement);
+    const LabelElement= document.createElement('label')
+    LabelElement.innerText  = questions[i].caption;
+    LiElement.append(LabelElement)
 
-const thirdLiElement=document.createElement('li')
-ulElement.append(thirdLiElement);
-
-const fourthLiElement=document.createElement('li')
-ulElement.append(fourthLiElement);
-
-/*------- label --------*/
-const oneLabelElement= document.createElement('label')
-oneLabelElement.innerText  = questions[0].caption;
-oneLiElement.append(oneLabelElement)
-
-const twoLabelElement= document.createElement('label')
-twoLabelElement.innerText  = questions[1].caption;
-twoLiElement.append(twoLabelElement)
-
-const thirdLabelElement= document.createElement('label')
-thirdLabelElement.innerText  = questions[2].caption;
-thirdLiElement.append(thirdLabelElement)
-
-const fourthLabelElement= document.createElement('label')
-fourthLabelElement.innerText  = questions[3].caption;
-fourthLiElement.append(fourthLabelElement)
+    const Checkbox= document.createElement('input')
+    LabelElement.append(Checkbox)
+    Checkbox.type="checkbox";
+}
 
 
-
-/*------- input --------*/
-const oneCheckbox= document.createElement('input')
-oneCheckbox.type="checkbox";
-oneLabelElement.append(oneCheckbox)
-
-const twoCheckbox= document.createElement('input')
-twoCheckbox.type="checkbox";
-twoLabelElement.append(twoCheckbox)
-
-const thirdCheckbox= document.createElement('input')
-thirdCheckbox.type="checkbox";
-thirdLabelElement.append(thirdCheckbox)
-
-const fourthCheckbox= document.createElement('input')
-fourthCheckbox.type="checkbox";
-fourthLabelElement.append(fourthCheckbox)
-
-
-
-/*-------  div and span --------*/
+/*-------  div , span --------*/
 const result = document.getElementsByClassName('result');
 const resultSpan = document.querySelector('span');
 
@@ -79,28 +44,32 @@ const resultSpan = document.querySelector('span');
 /*------- click  --------*/
 
 const arr= [];
+
 const buttonElement = document.querySelector('button')
+
+
+
 buttonElement.addEventListener("click", function f(event){
 
 
+    const checkBox = document.getElementsByTagName('input')
 
-    if ( oneCheckbox.checked === true) {
-        arr.push(+1)
-    }if(twoCheckbox.checked === false){
-        arr.push(+1)
-    }
-    if(thirdCheckbox.checked === false){
-        arr.push(+1)
-    }
-    if(fourthCheckbox.checked === true){
-        arr.push(+1)
+    for(let i = 0; i < questions.length; i++){
+
+        if (checkBox[i].checked === questions[i].correctAnswer){
+            arr.push(+1)
+        }
     }
 
-
-    const result = arr.reduce(function(sum, current) {
+    let result = arr.reduce(function(sum, current) {
         return sum + current;
     }, 0);
-    resultSpan.innerText = ` = ${result} / 4 `;
+
+    const resultLength = questions.length
+
+    resultSpan.innerText = ` = ${result} / ${resultLength} `;
+
+
 
     this.removeEventListener('click', f );
 
